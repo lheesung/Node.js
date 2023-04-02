219f4229c5da1590c935c26321191f0f05ee4099
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-04-02 17:52:48 +0900

    socket

---
+++ Socket/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use("/", function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080);
+
+const WebSocket = require('ws');
+
+const socket = new WebSocket.Server({
+    port: 8081
+});
+
+socket.on('connection', (ws, req) => {
+    ws.on('message', (msg) => {
+        console.log("request: " + msg);
+    })
+})
